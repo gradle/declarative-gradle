5ad68c0f531fb5e63c1edfb13d74c6c047abb370
Tom Tresansky <ttresansky@gradle.com>
2024-03-04 10:46:57 -0500

    Enable configuring JVM target

--- unified-prototype/unified-plugin/plugin-kmp/src/main/java/org/gradle/api/experimental/kmp/KmpJvmTarget.java
+++ unified-prototype/unified-plugin/plugin-kmp/src/main/java/org/gradle/api/experimental/kmp/KmpJvmTarget.java
@@ -3,6 +3,7 @@ package org.gradle.api.experimental.kmp;
 import org.gradle.api.provider.Property;
 import org.gradle.declarative.dsl.model.annotations.Restricted;
 
+@Restricted
 public interface KmpJvmTarget extends KmpTarget {
     @Restricted
     Property<Integer> getJdkVersion();
