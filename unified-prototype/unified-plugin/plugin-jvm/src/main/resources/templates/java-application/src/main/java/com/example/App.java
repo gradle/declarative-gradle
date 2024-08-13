1084af2e0132a116c0d3d49a12be146b4ea3967c
Tom Tresansky <ttresansky@gradle.com>
2024-08-13 09:53:18 -0400

    Fix project templates to be valid

--- unified-prototype/unified-plugin/plugin-jvm/src/main/resources/templates/java-application/src/main/java/com/example/App.java
+++ unified-prototype/unified-plugin/plugin-jvm/src/main/resources/templates/java-application/src/main/java/com/example/App.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import com.example.utils.Utils;
 import com.google.common.collect.ImmutableList;
 
 public class App {
@@ -8,10 +7,6 @@ public class App {
         // Verify that Guava is available
         ImmutableList.Builder<String> builder = ImmutableList.builder();
         builder.add("Hello from Java " + System.getProperty("java.version"));
-
-        // Verify that the Java library is available
-        Utils utils = new Utils();
-        builder.add(utils.getWelcome());
 
         ImmutableList<String> messages = builder.build();
         messages.forEach(System.out::println);
