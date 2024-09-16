72d827c3682674084c4fd34c74c790b0a97abb23
Tom Tresansky <ttresansky@gradle.com>
2024-09-16 11:38:10 -0400

    Add Android application with Empty Activity init spec
    # Conflicts:
    #	unified-prototype/unified-plugin/plugin-android/src/main/java/org/gradle/api/experimental/buildinit/AndroidProjectSource.java

---
+++ unified-prototype/unified-plugin/plugin-android/src/integTest/groovy/org/gradle/api/experimental/android/AndroidApplicationEmptyActivityInitProjectSpec.groovy
@@ -0,0 +1,8 @@
+package org.gradle.api.experimental.android
+
+class AndroidApplicationEmptyActivityInitProjectSpec extends AbstractProjectInitSpecification {
+    @Override
+    String getPluginId() {
+        return "org.gradle.experimental.android-application-empty-activity"
+    }
+}
