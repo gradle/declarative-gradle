41118f2fc58fe4b5dceb5290175799c9a8902739
Tom Tresansky <ttresansky@gradle.com>
2024-09-30 10:27:08 -0400

    Comment out init project generation types

--- unified-prototype/unified-plugin/plugin-kmp/src/main/java/org/gradle/api/experimental/buildinit/KMPProjectSource.java
+++ unified-prototype/unified-plugin/plugin-kmp/src/main/java/org/gradle/api/experimental/buildinit/KMPProjectSource.java
@@ -1,25 +1,26 @@
 package org.gradle.api.experimental.buildinit;
 
-import org.gradle.buildinit.projectspecs.InitProjectGenerator;
+//import org.gradle.buildinit.projectspecs.InitProjectGenerator;
 import org.gradle.buildinit.projectspecs.InitProjectSource;
-import org.gradle.buildinit.projectspecs.InitProjectSpec;
+//import org.gradle.buildinit.projectspecs.InitProjectSpec;
 
-import java.util.List;
+//import java.util.List;
 
 /**
  * A {@link InitProjectSource} of project specifications for Kotlin (JVM) projects.
  */
 @SuppressWarnings("UnstableApiUsage")
-public final class KMPProjectSource implements InitProjectSource{
-    @Override
-    public List<InitProjectSpec> getProjectSpecs() {
-        return List.of(
-            new StaticProjectSpec("kotlin-application", "Declarative Kotlin (JVM) Application Project")
-        );
-    }
-
-    @Override
-    public Class<? extends InitProjectGenerator> getProjectGenerator() {
-        return StaticProjectGenerator.class;
-    }
-}
+public final class KMPProjectSource {}
+// implements InitProjectSource {
+//    @Override
+//    public List<InitProjectSpec> getProjectSpecs() {
+//        return List.of(
+//            new StaticProjectSpec("kotlin-application", "Declarative Kotlin (JVM) Application Project")
+//        );
+//    }
+//
+//    @Override
+//    public Class<? extends InitProjectGenerator> getProjectGenerator() {
+//        return StaticProjectGenerator.class;
+//    }
+//}
