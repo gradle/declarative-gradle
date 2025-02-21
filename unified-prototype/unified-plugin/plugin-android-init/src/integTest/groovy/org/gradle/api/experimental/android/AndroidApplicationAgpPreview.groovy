42b537b221b5e98505467eb63cf6e4eb9f035d57
József Bartók <jbartok@gradle.com>
2025-02-21 09:30:24 +0200

    Ignore test until it can be fixed in Gradle

--- unified-prototype/unified-plugin/plugin-android-init/src/integTest/groovy/org/gradle/api/experimental/android/AndroidApplicationAgpPreview.groovy
+++ unified-prototype/unified-plugin/plugin-android-init/src/integTest/groovy/org/gradle/api/experimental/android/AndroidApplicationAgpPreview.groovy
@@ -1,5 +1,8 @@
 package org.gradle.api.experimental.android
 
+import spock.lang.Ignore
+
+@Ignore("Need to have following fix in Gradle: https://github.com/gradle/gradle/pull/32512") // TODO
 class AndroidApplicationAgpPreview extends AbstractAndroidBuildInitSpec {
     @Override
     protected String getProjectSpecType() {
