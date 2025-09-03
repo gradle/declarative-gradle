500f436b479333f2f5923d0585f1bd61fe278d49
Gary Hale <gary@gradle.com>
2025-09-03 06:25:09 -0400

    Fix zipslip issue in ResourceLoader

--- unified-prototype/unified-plugin/plugin-common/src/integTest/groovy/org/gradle/util/ResourceLoaderIntegrationTest.groovy
+++ unified-prototype/unified-plugin/plugin-common/src/integTest/groovy/org/gradle/util/ResourceLoaderIntegrationTest.groovy
@@ -1,10 +1,18 @@
 package org.gradle.util
 
 import spock.lang.Specification
+import spock.lang.TempDir
+
+import java.nio.file.InvalidPathException
+import java.util.jar.JarEntry
+import java.util.jar.JarOutputStream
 
 import static org.gradle.test.util.TestAssertions.assertDirContainsExactly
 
 class ResourceLoaderIntegrationTest extends Specification {
+    @TempDir
+    File tempDir
+
     private File outputDir = new File("build/tmp/integTest/output").tap {
         deleteDir()
         mkdirs()
@@ -19,5 +27,36 @@ class ResourceLoaderIntegrationTest extends Specification {
 
         then:
         assertDirContainsExactly(outputDir, ['build.gradle.dcl', 'src/main/java/com/example/lib/Library.java'])
+    }
+
+    def "throws exception when resource resolves to a file outside of the target directory"() {
+        given:
+        File poisonJar = createPoisonJarFile()
+        URLClassLoader poisonClassLoader = new URLClassLoader([poisonJar.toURI().toURL()] as URL[])
+        ResourceLoader resourceLoader = new ResourceLoader(poisonClassLoader)
+
+        when:
+        resourceLoader.extractDirectoryFromResources("foo", outputDir)
+
+        then:
+        def e = thrown(InvalidPathException)
+        e.message == "Entry resolves to a path outside of the target directory: /../poison.txt"
+
+        and:
+        !new File(outputDir.parentFile, "poison.txt").exists()
+    }
+
+    File createPoisonJarFile() {
+        File poisonJar = new File(tempDir, "poison.jar")
+        new JarOutputStream(new FileOutputStream(poisonJar)).withCloseable { jarOut ->
+            def foo = new JarEntry("foo/")
+            jarOut.putNextEntry(foo)
+            jarOut.closeEntry()
+            def poison = new JarEntry("foo/../poison.txt")
+            jarOut.putNextEntry(poison)
+            jarOut.write("This is poison".bytes)
+            jarOut.closeEntry()
+        }
+        return poisonJar
     }
 }
